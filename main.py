25ce56d84e1d3b170e41a7c84204286931f3f41b
Magmawalker24 <59489205+Magmawalker24@users.noreply.github.com>
2020-01-03 17:01:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+year = 0
+animal = 0
+number = 0
+element = 0
+yinyang = 0
+v = 0
+name = ["", ""]
 
+print("Enter the year (Must be in Gregorian calendar format and after 4 AD)")
+year = int(input())
